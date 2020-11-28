8f707dd2f9e67fef19482918cc51426ee48efdc9
mike591 <mikemach123@gmail.com>
2020-11-28 09:36:22 -0800

    add firebase and packages

---
+++ src/components/App/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="App">This is the drawing app</div>;
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "components/App";
+import firebase from "firebase/app";
+import "normalize.css";
+import "./main.scss";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDBUTth9seIVwqZ-u_j49N_e51w4Iui8oQ",
+  authDomain: "drawingtrainer-5ecd0.firebaseapp.com",
+  databaseURL: "https://drawingtrainer-5ecd0.firebaseio.com",
+  projectId: "drawingtrainer-5ecd0",
+  storageBucket: "drawingtrainer-5ecd0.appspot.com",
+  messagingSenderId: "275778725549",
+  appId: "1:275778725549:web:8fb9b0a312d2948f000824",
+  measurementId: "G-9XWPZX33MB",
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
