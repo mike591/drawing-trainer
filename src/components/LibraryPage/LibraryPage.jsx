1016ae13e42a50d0e0611149dd9db00f1af2236d
mike591 <mikemach123@gmail.com>
2020-12-19 11:29:34 -0800

    show drawings in library

--- src/components/LibraryPage/LibraryPage.jsx
+++ src/components/LibraryPage/LibraryPage.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { Container, Segment, Button, Icon } from "semantic-ui-react";
+import { privateRoutes } from "utils/routes";
+import { useHistory } from "react-router-dom";
+import { useAuth } from "hooks/useAuth";
+import { useDrawings } from "hooks/useDrawings";
 
 const LibraryPage = () => {
-  return <div>library page</div>;
+  const history = useHistory();
+  const { user } = useAuth();
+  const { drawings } = useDrawings(user);
+
+  return (
+    <Container className="LibraryPage">
+      <Button
+        className="return-button"
+        icon
+        onClick={() => history.push(privateRoutes.home.path)}
+      >
+        <Icon name="arrow left" />
+      </Button>
+      <Segment.Group>
+        {drawings.map((drawing, idx) => {
+          const prompt = `${drawing.adjective} ${drawing.noun}`;
+          return (
+            <div key={idx}>
+              <div>{prompt}</div>
+              <img src={drawing.imageUrl} alt={prompt} />
+            </div>
+          );
+        })}
+      </Segment.Group>
+    </Container>
+  );
 };
 
 export default LibraryPage;
