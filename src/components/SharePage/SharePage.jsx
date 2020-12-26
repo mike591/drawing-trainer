cdb40ef94337f8a469d1da8e5e9e0bdcbebdbf43
mike591 <mikemach123@gmail.com>
2020-12-26 10:44:13 -0800

    share page now show images

--- src/components/SharePage/SharePage.jsx
+++ src/components/SharePage/SharePage.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import { useQuery } from "hooks/useQuery";
 import { publicRoutes } from "utils/routes";
+import { useDrawing } from "hooks/useDrawing";
+import { Container, Card, Image } from "semantic-ui-react";
+import dayjs from "dayjs";
 
 const SharePage = () => {
   const history = useHistory();
@@ -11,7 +14,21 @@ const SharePage = () => {
     history.push(publicRoutes.login.path);
   }
 
-  return <div>share: {drawingID}</div>;
+  const { drawing } = useDrawing(drawingID);
+  const prompt = `${drawing.adjective || ""} ${drawing.noun || ""}`;
+  const date = dayjs.unix(drawing?.createdAt?.seconds);
+
+  return (
+    <Container className="SharePage">
+      <Card>
+        <Image src={drawing.imageUrl} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>{prompt}</Card.Header>
+          <Card.Meta>{date.format("DD/MM/YYYY hh:mm:ssa")}</Card.Meta>
+        </Card.Content>
+      </Card>
+    </Container>
+  );
 };
 
 export default SharePage;
